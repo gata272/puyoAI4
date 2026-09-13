c55fa7c272f402f0f8b7c9b947f3c94c4fb61d59
gata272 <t3871.teru@gmail.com>
2026-09-13 19:12:19 +0900

    Add files via upload

--- tests/test_weight_api.cpp
+++ tests/test_weight_api.cpp
@@ -5,7 +5,7 @@
 #include <limits>
 int main() {
     puyo::Weights w = puyo::amaBuildWeights();
-    assert(puyo::weightCount() == 17);
+    assert(puyo::weightCount() == 27);
     for (int i=0;i<puyo::weightCount();++i) {
         assert(puyo::weightName(i)[0] != '\0');
         double old = puyo::getWeight(w,i);
@@ -14,7 +14,7 @@ int main() {
         assert(std::abs(puyo::getWeight(w,i)-(old+1.25)) < 1e-9);
     }
     assert(!puyo::setWeight(w,-1,1));
-    assert(!puyo::setWeight(w,17,1));
+    assert(!puyo::setWeight(w,27,1));
     assert(!puyo::setWeight(w,0,std::numeric_limits<double>::quiet_NaN()));
     std::cout << "weight API test passed\n";
 }
