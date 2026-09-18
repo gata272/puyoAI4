c7620551167d3d1efad414a82e2c794c4f968d23
gata272 <t3871.teru@gmail.com>
2026-09-18 20:00:09 +0900

    Add files via upload

--- tests/test_chain_benchmark.cpp
+++ tests/test_chain_benchmark.cpp
@@ -13,12 +13,15 @@ int main() {
     config.progress = false;
 
     const std::string json = puyo::runChainBenchmark(config);
-    assert(json.find("\"version\":2") != std::string::npos);
+    assert(json.find("\"version\":3") != std::string::npos);
     assert(json.find("\"games\":3") != std::string::npos);
     assert(json.find("\"turns\":8") != std::string::npos);
     assert(json.find("\"gameOverReasons\":{") != std::string::npos);
     assert(json.find("\"no_safe_move\":") != std::string::npos);
     assert(json.find("\"selected_death_with_safe_move\":") != std::string::npos);
     assert(json.find("\"averageTurns\":") != std::string::npos);
+    assert(json.find("\"diagnosticHistory\":5") != std::string::npos);
+    assert(json.find("\"averageSafeMovesBeforeDeath\":[") != std::string::npos);
+    assert(json.find("\"diagnosticCounts\":[") != std::string::npos);
     return 0;
 }
