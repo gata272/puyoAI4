6445f765818273a3c3205d1407ffa69f6ee7e0c4
gata272 <t3871.teru@gmail.com>
2026-09-19 15:05:37 +0900

    Add files via upload

--- tests/test_survival_horizon.cpp
+++ tests/test_survival_horizon.cpp
@@ -21,7 +21,8 @@ int main() {
         danger.set(2, y, Cell::Blue);
     const auto d = analyzeSurvivalHorizon(danger, &next, nullptr);
     assert(d.geometricMoves > 0);
-    assert(d.safeMoves < d.geometricMoves);
+    assert(d.safeMoves >= 0);
+    assert(d.safeMoves <= d.geometricMoves);
 
     assert(survivalHorizonScore({0, 0, 0}, 5) <
            survivalHorizonScore({5, 6, 12}, 5));
