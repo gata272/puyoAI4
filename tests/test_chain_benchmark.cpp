f9397ba384dc8d5c4df690b939e7cec7d04549f0
gata272 <t3871.teru@gmail.com>
2026-09-19 20:11:49 +0900

    Add files via upload

--- tests/test_chain_benchmark.cpp
+++ tests/test_chain_benchmark.cpp
@@ -5,17 +5,17 @@
 
 int main() {
     puyo::ChainBenchmarkConfig config;
-    config.games = 3;
-    config.turns = 8;
+    config.games = 1;
+    config.turns = 3;
     config.seed = 12345;
     config.depth = 2;
     config.beamWidth = 4;
     config.progress = false;
 
     const std::string json = puyo::runChainBenchmark(config);
-    assert(json.find("\"version\":3") != std::string::npos);
-    assert(json.find("\"games\":3") != std::string::npos);
-    assert(json.find("\"turns\":8") != std::string::npos);
+    assert(json.find("\"version\":4") != std::string::npos);
+    assert(json.find("\"games\":1") != std::string::npos);
+    assert(json.find("\"turns\":3") != std::string::npos);
     assert(json.find("\"gameOverReasons\":{") != std::string::npos);
     assert(json.find("\"no_safe_move\":") != std::string::npos);
     assert(json.find("\"selected_death_with_safe_move\":") != std::string::npos);
@@ -23,5 +23,10 @@ int main() {
     assert(json.find("\"diagnosticHistory\":5") != std::string::npos);
     assert(json.find("\"averageSafeMovesBeforeDeath\":[") != std::string::npos);
     assert(json.find("\"diagnosticCounts\":[") != std::string::npos);
+    assert(json.find("\"recordDecisionLog\":true") != std::string::npos);
+    assert(json.find("\"decisionLog\":[") != std::string::npos);
+    assert(json.find("\"gameSummaries\":[") != std::string::npos);
+    assert(json.find("\"preBoard\":\"") != std::string::npos);
+    assert(json.find("\"debugLog\":\"") != std::string::npos);
     return 0;
 }
